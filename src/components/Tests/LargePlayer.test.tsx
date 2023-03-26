44bca47683a3e7c97d2bd2cabeff95245054212f
Omar Mbengue <ombengue@sqli.com>
2023-03-26 15:57:22 +0000

    Refacto : improve code modularity

--- src/components/Tests/LargePlayer.test.tsx
+++ src/components/Tests/LargePlayer.test.tsx
@@ -1,28 +1,6 @@
 import LargePlayerCard from '../LargePlayerCard'
 import { render, screen } from '@testing-library/react'
-
-const player = {
-    id: 52,
-    firstname: 'Novak',
-    lastname: 'Djokovic',
-    shortname: 'N.DJO',
-    sex: 'M',
-    country: {
-        picture:
-            'https://data.latelier.co/training/tennis_stats/resources/Serbie.png',
-        code: 'SRB',
-    },
-    picture:
-        'https://data.latelier.co/training/tennis_stats/resources/Djokovic.png',
-    data: {
-        rank: 2,
-        points: 2542,
-        weight: 80000,
-        height: 188,
-        age: 31,
-        last: [1, 1, 1, 1, 1],
-    },
-}
+import { player } from '../../data/player'
 
 describe('LargePlayerCard', () => {
     test('should renders player card with informations passed', () => {
--- src/components/Tests/PlayerCard.test.tsx
+++ src/components/Tests/PlayerCard.test.tsx
@@ -1,29 +1,7 @@
 import PlayerCard from '../PlayerCard'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-
-const player = {
-    id: 52,
-    firstname: 'Novak',
-    lastname: 'Djokovic',
-    shortname: 'N.DJO',
-    sex: 'M',
-    country: {
-        picture:
-            'https://data.latelier.co/training/tennis_stats/resources/Serbie.png',
-        code: 'SRB',
-    },
-    picture:
-        'https://data.latelier.co/training/tennis_stats/resources/Djokovic.png',
-    data: {
-        rank: 2,
-        points: 2542,
-        weight: 80000,
-        height: 188,
-        age: 31,
-        last: [1, 1, 1, 1, 1],
-    },
-}
+import { player } from '../../data/player'
 
 describe('PlayerCard', () => {
     const setSelectedPlayerIndex = jest.fn()
